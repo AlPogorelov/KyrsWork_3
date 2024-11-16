32c501eacba459f177e277ee08cbc2b4865a05b7
Al Pogorelov <eczemplrez@gmail.com>
2024-11-16 18:47:47 +0600

    init and done

---
+++ main.py
@@ -0,0 +1,46 @@
+import psycopg2
+
+from src.DBManager import DBManager
+from src.api_employers import HeadHunterAPIEmployers
+
+
+
+
+def start_test_program():
+
+    hh_api = HeadHunterAPIEmployers()
+
+    employers = {"1C-Рарус", "Почта банк", 'DNS Технологии', 'В2В-Персонал', 'Т-банк',
+                 'Газпром нефть', 'ПАО «Россети Сибирь»', 'Тензор', 'ВТБ Банк', 'Красное & Белое, розничная сеть'}
+
+
+
+
+    employers_id = hh_api.get_employer_id(employers)
+
+    print(employers_id)
+
+    vac_list_by_empl = hh_api.get_vac_by_employer(employers_id)
+
+    db = DBManager()
+
+    db.save_date_to_database(employers_id, vac_list_by_empl)
+
+    count = db.get_companies_and_vacancies_count()
+
+    all_vac = db.get_all_vacancies()
+
+    avg = db.get_avg_salary()
+
+    hi_sal = db.get_vacancies_with_higher_salary
+
+    keyword ='зам'
+
+    key_vac = db.get_vacancies_with_keyword(keyword)
+
+
+
+
+
+if __name__ == '__main__':
+    start_test_program()
